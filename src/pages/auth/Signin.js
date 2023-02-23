2b851f3e7975bbb9558210cff8aacc6896253f66
Ghost <culestfrosh@gmail.com>
2023-02-23 23:28:35 +0100

    signin page

---
+++ src/pages/auth/Signin.js
@@ -0,0 +1,19 @@
+
+import {
+  useState,
+  useEffect,
+  useReducer
+} from "react"
+import {
+  Paper,
+  Card,
+  Typography,
+  Button
+} from "@mui/material"
+const Signin = () =>{
+  return(
+    <Paper>
+    </Paper>
+    )
+}
+export default Signin
