0888e036bbbd1ff9e9c859cf73376076ffac0a08
Ghost <culestfrosh@gmail.com>
2023-02-23 23:29:42 +0100

    Header file

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import {
+  Paper,
+  Card,
+  Typography,
+  Button
+} from "@mui/material"
+import {
+  useState,
+  useEffect,
+  useRef
+} from "react"
+
+const Header = () => {
+  return (
+    <Paper>
+    </Paper>
+    )
+}
+
+
+export default Header
