f521628a6be6b0e9814664c17a145cb3491e3973
Ghost <culestfrosh@gmail.com>
2023-02-23 23:29:22 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
-
+// useLoaderData that where the id is
+// alao a redirect
 import './App.css';
+
+import Logout from "./Components/Logout"
+import Header from "./Components/Header"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./pages/Error"
+const router = createBrowserRouter([
+  {
+    path: "/logout",
+    element: <Logout/>,
+    errorElement: ErrorPage
+  },
+  /*{
+    path: "/signin",
+    element: <Signin/>,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/signup",
+    element: <signup/>,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/doctors",
+    element: <Doctors />,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/doctors/id",
+    element: <Doctor/>,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/appointments",
+    element: <Appointments/>,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/appointments/id",
+    element: <Appointment/>,
+    errorElement: ErrorPage
+  },
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: ErrorPage
+  },*/
+]);
 
 function App() {
   return (
     <div className="">
-      
+       <Header />
+       <RouterProvider router={router}/>
     </div>
   );
 }
