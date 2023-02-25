1ea360ce2a068c7c885a048be3c763148f6bdcb5
Ghost <culestfrosh@gmail.com>
2023-02-25 22:07:31 +0100

    Added styles and link to go back home

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,16 +1,21 @@
-import { useRouteError } from "react-router-dom";
-
+import { 
+  useRouteError,
+  Link
+ } from "react-router-dom";
+import { Card,Paper, Typography} from "@mui/material"
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" className="h-screen w-screen flex justify-center align-center text-center">
+    <Paper className="h-1/2 w-4/5 rounded-xl shadow-3xl">
+      <Typography variant="h1">Oops!</Typography>
+      <Typography variant="h6">Sorry, an unexpected error has occurred.</Typography>
+      <Typography variant="h6">
+        <Link to="/">Go back home</Link>
+      </Typography>
+      </Paper>
     </div>
   );
 }
