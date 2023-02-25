33b03a476559011c1b444d6f8fef251900ebe474
Ghost <culestfrosh@gmail.com>
2023-02-25 22:11:45 +0100

    search bar

---
+++ src/Components/Search.js
@@ -0,0 +1,38 @@
+import {
+  TextField,
+  Button
+} from "@mui/material"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
+const Search = () =>{
+  const [search, setSearch] = useState(null)
+  const navigate = useNavigate;
+  return (
+    <Box component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '80vw', border:'none'},
+      }}
+      className="shdow-2xl"
+      noValidate
+      autoComplete="on">
+      <TextField
+    id="search"
+    label="Search"
+    placeholder="What type of doctor are you looking for?"
+    className="bg-blue-100 border-none rounded-2xl" 
+     InputProps={{
+     value:search,
+     onChange:(e)=>{
+       setSearch(e.target.value)
+     },
+     }}
+     />
+     <Button variant="contained" onClick{()=>{
+       navigate(`search/${search}`)
+     }}>🔍</Button>
+    </Box>
+    )
+}
+
+export default Search
