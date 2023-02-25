7202447305ba8a3d7298bb0fa348217c2231e5f3
Ghost <culestfrosh@gmail.com>
2023-02-25 22:08:02 +0100

    doctors index page

---
+++ src/pages/doctors/Index.js
@@ -0,0 +1,19 @@
+import {
+  useSelector
+} from "react-redux";
+import {
+  Card
+} from "@mui/material";
+import {
+  useEffect
+} from "react";
+
+
+
+const Doctors = () =>{
+  return(
+    <div>helo</div>
+    )
+}
+
+export default Doctors
