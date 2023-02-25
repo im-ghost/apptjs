f422ea3f218c72a96ba51e9c1cec6e792a4d4fee
Ghost <culestfrosh@gmail.com>
2023-02-25 15:01:01 +0100

    added the user state to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/users/usersSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    users: userReducer,
   },
 });
