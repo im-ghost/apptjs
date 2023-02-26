e5987392a8a3846137cd62630ebd4f01b83baea6
Ghost <culestfrosh@gmail.com>
2023-02-26 18:12:53 +0100

    wrote code

---
+++ src/Components/Doctor.js
@@ -0,0 +1,25 @@
+
+import {
+  Typography
+} from "@mui/material"
+
+import { Link , BrowserRouter as Router } from "react-router-dom"
+
+
+export default function Doctor({doctor}){
+  return(
+    <div className="shadow-3xl rounded-xl ">
+     <Router>
+      <Link to={`/doctors/${doctor._id}`}>
+       <figure className="">
+         <img className="h-[70%] shadow rounded-2xl" src="%PUBLIC_URL%/logo192.png" alt="Doctor"/>
+         <figcaption className="">
+           <Typography variant="h5">{doctor.name}</Typography>
+           <Typography variant="h6">{doctor.specialities}</Typography>
+          </figcaption>
+       </figure>
+      </Link>
+     </Router>
+    </div>
+    )
+}
