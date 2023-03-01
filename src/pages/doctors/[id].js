30659c447aa9b547b401a88f0fa9671756051b23
Ghost <culestfrosh@gmail.com>
2023-03-01 18:28:40 +0100

    doctor page

---
+++ src/pages/doctors/[id].js
@@ -0,0 +1,23 @@
+import {
+  Typography
+} from "@mui/material";
+import {
+  getDoctorById
+} from "../../features/users/usersSlice"
+import {
+  Link,
+  BrowserRouter as Router,
+  useNavigate
+} from "react-router-dom"
+import {
+  useSelector,
+  useDispatch
+} from "react-redux"
+
+import {
+  useEffect
+} from "react"
+const Doctor = () =>{
+  return()
+}
+export default Doctor
