8596ab846e9df943fb50f71f5420e7f4715fbfdc
Ghost <culestfrosh@gmail.com>
2023-03-02 15:56:51 +0100

    removed the API function

--- src/app/hooks.js
+++ src/app/hooks.js
@@ -1,30 +1,3 @@
-
-async function request(
-  url, 
-  config = {}
-) {
-  const response = await fetch(url, config);
-  return await response.json();
+const getDoctorAvailableSlots = () =>{
+  
 }
- export const api = {
-  get: (url,token) => 
-  	request(url,{
-  	  headers:{
-  	    authorization:token
-  	  }
-  	}),
-  post: (url, body, token) => 
-  	request(url, { method: 'POST',   headers:{
-  	    authorization:token
-  	  },body }),
-   put: (url, body,token) => 
-  	request(url, { method: 'PUT', body,  headers:{
-  	    authorization:token
-  	  } }),
-  delete: (url,id,token) => 
-  	request(url, { method: 'DELETE',  headers:{
-  	    authorization:token
-  	  }, body:id }),
-}
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
