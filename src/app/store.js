45579b079767e269b29c4e6a10c246c8d1716506
Ghost <culestfrosh@gmail.com>
2023-03-02 15:57:13 +0100

    added appt reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/users/usersSlice';
+import apptReducer from '../features/appt/apptSlice';
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
+    appt: apptReducer
   },
 });
