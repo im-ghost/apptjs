02cb277929be745921d12dfd5dce4427c1f94454
Ghost <culestfrosh@gmail.com>
2023-03-06 19:50:16 +0100

    all should route to home page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,16 +10,16 @@ const Footer = () =>{
   const navigate = useNavigate()
   return (
     <div className="rounded-3xl flex flex-row w-[80vw]  space-evenly  shadow-3xl absolute bottom-5 p-4 m-2 space-x-8 justify-items-stretch">
-    <Typography variant="h4" onClick={()=> navigate("")}>
+    <Typography variant="h4" onClick={()=> navigate("/")}>
  
     🏠
     </Typography>
-    <Typography variant="h4" onClick={()=> navigate("")}>
+    <Typography variant="h4" onClick={()=> navigate("/")}>
  
     📅
     </Typography>
   
-    <Typography variant="h4" onClick={()=> navigate("")}>
+    <Typography variant="h4" onClick={()=> navigate("/")}>
     🙎
   
     </Typography>
