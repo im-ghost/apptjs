4a0cd163eed1948491cff10d2334e0bb8b096fe9
Ghost <culestfrosh@gmail.com>
2023-03-08 18:15:50 +0100

    create appointment

---
+++ src/pages/CreateAppt.js
@@ -0,0 +1,113 @@
+/*
+import {
+  useEffect,
+  useState
+} from "react"
+import  {
+  useNavigate,
+  useParams
+} from "react-router-dom"
+import { createAppointment } from "../../features/appt/apptSlice"
+
+import { 
+  useDispatch,
+  useSelector
+ } from "react-redux"
+import {
+  Typography,
+  Box,
+  Button,
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel
+ 
+} from "@mui/material"
+
+const CreateAppt =()=>{
+ const [err,setError] = useState("")
+ const [time,setTime] = useState("")
+ const [times,setTimes] = useState("")
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+ const { id} = useParams()
+  const { user , doctors } = useSelector(
+    (state) => state.users
+  )
+  const {
+    doctorTimeSlots,
+    timeSlots,
+    createAppointment,
+    isSuccessful,
+    error
+  } = useSelector(state=>state.appt)
+  useEffect(() => {
+    if (error) {
+      setError(error.message)
+    }
+    if (isSuccess) {
+      navigate('/')
+    }
+  }, [navigate,error, isSuccess,user])
+  useEffect(()=>{
+    if(doctors){
+        const doctor = doctors.find(doc=>doc._id === id)
+       if(doctor !== undefined) {
+         setDoctor(doctor)
+         dispatch(doctorTimeSlots(id))
+       }
+       else navigate("/")
+    }
+  },[doctors,dispatch])
+  useEffect(()=>{
+    if(timeSlots){
+      setTimes(timeSlots)
+    }
+  },[timeSlots, dispatch])
+  const create = async (e)=>{
+    e.preventDefault();
+   const data = {
+     name:Date.now(),
+     userId:user._id,
+     doctorId:doctor._id,
+     time:time
+   }
+   await dispatch(createAppointment(data))
+  } 
+
+  return(
+    <div className="bg-blue-900 text-center flex flex-col justify-center  h-screen w-screen rounded-2xl border-none shadow-3xl">
+    <Typography variant='h2'>
+    Create an appointment with {doctor.name}
+    </Typography>
+    <Typography variant='h3' className="bg-red-900">
+    {err}
+    </Typography>
+      <Box>
+      <FormControl fullWidth>
+  <InputLabel>Time Slots</InputLabel>
+  <Select
+    labelId="time slots"
+    value={time}
+    label="Age"
+    onChange={(e)=>setTime(e.target.value)}
+  >
+  {times.map(tim)=>(
+    <MenuItem value={tim}>{tim}</MenuItem>
+    )}
+  </Select>
+</FormControl>
+     <Button type='submit' onClick={create} className="sign__button">
+     <Typography variant="h5">
+     Create
+     </Typography>
+     </Button>
+    </Box>
+    </div>
+    )
+   }
+
+
+export default CreateAppt;
+
+*/
